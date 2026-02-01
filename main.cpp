f87f6f1568f0be1f87563a1997fc68d46bfdc80a
collin <2672997026@qq.com>
2026-02-01 11:50:04 +0800

    first v

--- main.cpp
+++ main.cpp
@@ -2,21 +2,10 @@
 #include <vector>
 #include <ranges>
 #include <queue>
-#include <opencv2/opencv.hpp>
+
 using namespace std;
 static queue<int> q;
-int main(void) {
-    std::cout << "Hello, World!" << std::endl;
-    vector<int> a = {1, 2, 3, 4, 5};
-    for(auto [index,valuue]: views::enumerate(a)) {
-        q.emplace(valuue);
-        cout << index << ":" << valuue << endl;
-    }
-    std::cout << "-----" << std::endl;
-    printf("queue size:%lu\n",q.size());
-    for(auto[index, valuue]: views::enumerate(a)) {
-        cout << index << ":" << valuue << endl;
-        q.pop();
-        }
+int main() {
+
         return 0;
 }
